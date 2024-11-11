b66181c630235d848e5ad7c04c312b223251d0f0
greg4fun <gstencel@gmail.com>
2024-11-11 20:18:27 +0000

    banking

--- banking/views.py
+++ banking/views.py
@@ -2,11 +2,29 @@ from rest_framework import viewsets
 from .models import Account, Transaction, Business
 from .serializers import AccountSerializer, TransactionSerializer, BusinessSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
     permission_classes = [IsAuthenticated]
+    
+    @action(detail=True, methods=['get'])
+    def roundups(self, request, pk=None):
+        account = self.get_object()
+        roundups = Transaction.objects.filter(from_account=account, transaction_type='payment')
+        savings = sum([round(roundup.amount - int(roundup.amount)) for roundup in roundups])
+        return Response({'savings': savings})
+
+    @action(detail=True, methods=['get'])
+    def spending_trends(self, request, pk=None):
+        account = self.get_object()
+        payments = Transaction.objects.filter(from_account=account, transaction_type='payment')
+        trends = payments.values('to_account__name').annotate(total=models.Sum('amount'))
+        return Response(trends)
+    
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
